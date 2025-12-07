bd2f15f5426a09fe62c1d7bbb333a8754b7ca609
Choudhary Khushboo GirdhariRam <khushboo@Choudharys-MacBook-Pro-M3.local>
2025-12-07 05:58:01 +0530

    Initial commit - RideShare project

---
+++ src/test/java/com/choudharykhushboo/RideShare/RideShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choudharykhushboo.RideShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
